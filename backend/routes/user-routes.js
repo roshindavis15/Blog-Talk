1d6f1c5afacb37712b1cbda024dcb456e86d6975
roshin <roshindavis15@gmail.com>
2024-06-13 14:48:23 +0530

    otp verification and login route

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { signUpValidation } from '../middleware/validation.js';
-import {getAllUsers,signUp} from '../controllers/user-controller.js';
+import {getAllUsers,signUp,verifyOTP,login} from '../controllers/user-controller.js';
 
 
 const userRouter=express.Router();
@@ -8,6 +8,10 @@ const userRouter=express.Router();
 userRouter.get('/',getAllUsers);
 
 userRouter.post('/signup',signUpValidation,signUp)
+
+userRouter.post('/verify-otp',verifyOTP);
+
+userRouter.post('/login',login)
 
 
 
